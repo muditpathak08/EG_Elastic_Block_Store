034764c998f99b84d2f661033d3ddeb60cb2ee2e
Mudit Pathak <mudit.pathak@coforge.com>
2023-11-06 14:12:58 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_ebs_volume" "project-iac-ebs" {
+  # count = "${var.create ? length(var.azs) * var.ebs_volumes : 0}"
+  count = var.ebs_volumes
+  availability_zone = var.azs
+  encrypted         = var.encrypted
+  # iops              = "${var.iops}"
+  size              = var.size
+  snapshot_id       = var.snapshot_id
+  type              = var.type
+  kms_key_id        = var.kms_key_id
+
+  tags = merge(tomap(var.ebs_volume_tags),{ApplicationOwner = var.ApplicationOwner, 
+      ApplicationTeam= var.ApplicationTeam, BackupSchedule = var.BackupSchedule, BusinessTower=var.BusinessTower
+      ServiceCriticality = var.ServiceCriticality})
+}
---
+++ terraform.tfvars
@@ -0,0 +1,12 @@
+region="us-east-2"
+Environment="Dev"
+size = "20"
+ApplicationOwner="aabc@hotmail.com"
+ApplicationTeam="Team1"
+BackupSchedule="DR7y"
+BusinessTower="abc@gmail.com"
+ServiceCriticality="High"
+
+
+
+
